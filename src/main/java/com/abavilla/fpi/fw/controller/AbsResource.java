2ad5f32c06a90162992405481c862ad46ccf77f1
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-09-08 16:05:16 +0800

    Initial Framework

---
+++ src/main/java/com/abavilla/fpi/fw/controller/AbsResource.java
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * FPI Application - Abavilla                                                 *
+ * Copyright (C) 2022  Vince Jerald Villamora                                 *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
+ ******************************************************************************/
+
+package com.abavilla.fpi.fw.controller;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.abavilla.fpi.fw.dto.IDto;
+import com.abavilla.fpi.fw.entity.AbsItem;
+import com.abavilla.fpi.fw.service.AbsSvc;
+import io.smallrye.mutiny.Multi;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public abstract class AbsResource<E extends IDto, I extends AbsItem, S extends AbsSvc<E,I>> implements IResource<I> {
+  @Inject
+  protected S service;
+
+  @GET
+  public Multi<E> getAll() {
+    return service.list();
+  }
+
+  @Override
+  public I save() {
+    return null;
+  }
+}
