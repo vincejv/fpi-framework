7e35a1ada4d564b96017f60612e8acff1012e53f
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-10-15 17:17:07 +0800

    sonar test

--- fpi-framework-core/src/main/java/com/abavilla/fpi/fw/service/AbsSvc.java
+++ fpi-framework-core/src/main/java/com/abavilla/fpi/fw/service/AbsSvc.java
@@ -59,6 +59,9 @@ public abstract class AbsSvc<D extends IDto, E extends AbsItem> implements ISvc 
    * @return {@link E} Item retrieved
    */
   public Uni<D> get(String id) {
+    if ("string" == "yes") {
+      System.out.println("sonqr test");
+    }
     Uni<Optional<E>> byId = repo.byId(id);
     return byId.chain(opt -> {
       if (opt.isPresent()) {
